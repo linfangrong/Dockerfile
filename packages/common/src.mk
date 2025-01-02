b56cef6675eff168d7caa87bfb86bbe05e395b6f
linfangrong <linfangrong.liuxin@qq.com>
2025-01-02 17:15:41 +0800

    [ADD] FFmpeg-n5.1.1

--- packages/common/src.mk
+++ packages/common/src.mk
@@ -1,19 +1,19 @@
-src_download_test:
+src_download_done:
 	( if [ ! -f ${SRC_NAME}.${SRC_ARCHIVE_SUFFIX} ]; then \
 		wget -t5 --timeout=20 --no-check-certificate -O ${SRC_NAME}.${SRC_ARCHIVE_SUFFIX} ${SRC_URL}; \
 	fi )
 
-src_extract_test:
+src_extract_done:
 	( if [ ! -d ${SRC_NAME} ]; then \
-		if [ ${SRC_ARCHIVE_SUFFIX} = "tar.xz" ]; then \
-			tar -xJf ${SRC_NAME}.${SRC_ARCHIVE_SUFFIX}; \
-		else \
+		if [ "${SRC_ARCHIVE_SUFFIX}" = "tar.xz" ]; then \
 			tar -xzf ${SRC_NAME}.${SRC_ARCHIVE_SUFFIX}; \
+		else \
+			tar -xJf ${SRC_NAME}.${SRC_ARCHIVE_SUFFIX}; \
 		fi; \
 		make patch; \
 	fi )
 
-src_config_test:
+src_config_done:
 	( if [ -f ./config_done ]; then \
 		echo "${SRC_NAME} the same configuration"; \
 	else \
@@ -21,5 +21,6 @@ src_config_test:
 	fi )
 
 clean:
+	rm -rf build-${SRC_NAME}
 	rm -rf ${SRC_NAME}
 	rm -rf config_done
